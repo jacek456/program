48c94cb4e5e2df374cd80525c61e4e3ec847a5f9
jacek surma <jacksur@wp.pl>
2023-09-17 16:12:56 +0200

    gotowe

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿string imie = "Ewa";
+int wiek = 13;
+string płeć = "mężczyzna";
+if (wiek <= 30 && płeć == "Kobieta")
+{
+    Console.WriteLine("Kobieta poniżej 30lat");
+}
+else if (imie == "Ewa" && wiek == 33) 
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if (płeć =="mężczyzna"&&wiek <18)
+{
+    Console.WriteLine("Jestem młodym mężczyzną");
+}
