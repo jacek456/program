480b592087ff7d1af111fc3478a4093b1e8819d1
jacek456 <jacksur@wp.pl>
2023-09-24 18:54:39 +0200

    coś

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,16 +1,70 @@
-﻿string name = "Ewa";
-string sex = "Kobieta";
-int age = 33;
-if (age <= 30 && sex == "Kobieta")
-{
-    Console.WriteLine("kobieta poniżej 30 lat");
-}
-else if (name == "Ewa" && age == 33)
+﻿int number = 202354392;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToCharArray();
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+foreach (char letter in letters)
+{   if (letter == '0')
+    {
+        counter0++;
 
-{
-    Console.WriteLine("ewa lat 33");
+
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+
+    {
+        counter3++;
+
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
 }
-else if (age < 18 && sex == "mężczyzna")
-{
-    Console.WriteLine("nie pełnoletni mężczyzna");
-}
+Console.WriteLine(counter0);
+Console.WriteLine(counter1);
+Console.WriteLine(counter2);
+Console.WriteLine(counter3);
+Console.WriteLine(counter4);
+Console.WriteLine(counter5);
+Console.WriteLine(counter6);
+Console.WriteLine(counter7);
+Console.WriteLine(counter8);
+Console.WriteLine(counter9);
+
